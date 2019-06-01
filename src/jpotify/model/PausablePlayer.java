52711886fd8a6549e542f2ee8cd7944468d7b17b
Radin Shayanfar <radin.shayanfar@gmail.com>
2019-06-02 00:25:21 +0430

    Pausable class added.

---
+++ src/jpotify/model/PausablePlayer.java
@@ -0,0 +1,140 @@
+package jpotify.model;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.AudioDevice;
+import javazoom.jl.player.Player;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class PausablePlayer {
+
+    private final static int NOTSTARTED = 0;
+    private final static int PLAYING = 1;
+    private final static int PAUSED = 2;
+    private final static int FINISHED = 3;
+
+    // the player actually doing all the work
+    private final Player player;
+
+    // locking object used to communicate with player thread
+    private final Object playerLock = new Object();
+
+    // status variable what player thread is doing/supposed to do
+    private int playerStatus = NOTSTARTED;
+
+    // keeping position to invoke label updater
+    private long lastInvokedTimePosition;
+
+    public PausablePlayer(final InputStream inputStream) throws JavaLayerException {
+        this.player = new Player(inputStream);
+    }
+
+    public PausablePlayer(final InputStream inputStream, final AudioDevice audioDevice) throws JavaLayerException {
+        this.player = new Player(inputStream, audioDevice);
+    }
+
+    /**
+     * Starts playback (resumes if paused)
+     */
+    public void play() {
+        synchronized (playerLock) {
+            switch (playerStatus) {
+                case NOTSTARTED:
+                    final Runnable r = this::playInternal;
+                    final Thread t = new Thread(r);
+                    t.setDaemon(true);
+                    t.setPriority(Thread.MAX_PRIORITY);
+                    playerStatus = PLAYING;
+                    t.start();
+                    break;
+                case PAUSED:
+                    resume();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Pauses playback. Returns true if new state is PAUSED.
+     */
+    public boolean pause() {
+        synchronized (playerLock) {
+            if (playerStatus == PLAYING) {
+                playerStatus = PAUSED;
+            }
+            return playerStatus == PAUSED;
+        }
+    }
+
+    /**
+     * Resumes playback. Returns true if the new state is PLAYING.
+     */
+    public boolean resume() {
+        synchronized (playerLock) {
+            if (playerStatus == PAUSED) {
+                playerStatus = PLAYING;
+                playerLock.notifyAll();
+            }
+            return playerStatus == PLAYING;
+        }
+    }
+
+    /**
+     * Stops playback. If not playing, does nothing
+     */
+    public void stop() {
+        synchronized (playerLock) {
+            playerStatus = FINISHED;
+            playerLock.notifyAll();
+        }
+    }
+
+    private void playInternal() {
+        lastInvokedTimePosition = 0;
+        while (playerStatus != FINISHED) {
+            try {
+                if (!player.play(1)) {
+                    break;
+                }
+            } catch (final JavaLayerException e) {
+                break;
+            }
+            int position = player.getPosition();
+            if (position - lastInvokedTimePosition >= 1000) {
+//                TODO: invoke label updater
+                lastInvokedTimePosition = position;
+                System.err.println(lastInvokedTimePosition);
+            }
+            // check if paused or terminated
+            synchronized (playerLock) {
+                while (playerStatus == PAUSED) {
+                    try {
+                        playerLock.wait();
+                    } catch (final InterruptedException e) {
+                        // terminate player
+                        break;
+                    }
+                }
+            }
+        }
+//        TODO: invoke song finished method
+        close();
+    }
+
+    /**
+     * Closes the player, regardless of current state.
+     */
+    public void close() {
+        synchronized (playerLock) {
+            playerStatus = FINISHED;
+        }
+        try {
+            player.close();
+        } catch (final Exception e) {
+            // ignore, we are terminating anyway
+        }
+    }
+}
