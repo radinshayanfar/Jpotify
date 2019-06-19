6834c0eb31884f23a9d0b0e55dd80698371e136e
Radin Shayanfar <radin.shayanfar@gmail.com>
2019-06-19 15:38:19 +0430

    Player now accepts pause and seek.

--- src/jpotify/model/CustomPlayer.java
+++ src/jpotify/model/CustomPlayer.java
@@ -1,36 +1,52 @@
 package jpotify.model;
 
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.AudioDevice;
 import javazoom.jl.player.Player;
 
-import java.io.InputStream;
+import java.io.*;
 
-public class PausablePlayer {
+public class CustomPlayer {
 
-    private final static int NOTSTARTED = 0;
+    private final static int NOT_STARTED = 0;
     private final static int PLAYING = 1;
     private final static int PAUSED = 2;
     private final static int FINISHED = 3;
 
     // the player actually doing all the work
-    private final Player player;
+    private Player player;
+
+    // song duration in milliseconds
+    private final long duration;
+
+    // song frames count
+    private final long frameCount;
+
+    // song file object
+    final private File file;
 
     // locking object used to communicate with player thread
     private final Object playerLock = new Object();
 
     // status variable what player thread is doing/supposed to do
-    private int playerStatus = NOTSTARTED;
+    private int playerStatus = NOT_STARTED;
 
     // keeping position to invoke label updater
     private long lastInvokedTimePosition;
 
-    public PausablePlayer(final InputStream inputStream) throws JavaLayerException {
-        this.player = new Player(inputStream);
+    public CustomPlayer(final File file) throws JavaLayerException, IOException, InvalidDataException, UnsupportedTagException {
+        this(file, null);
     }
 
-    public PausablePlayer(final InputStream inputStream, final AudioDevice audioDevice) throws JavaLayerException {
-        this.player = new Player(inputStream, audioDevice);
+    public CustomPlayer(final File file, final AudioDevice audioDevice) throws JavaLayerException, IOException, InvalidDataException, UnsupportedTagException {
+        this.file = file;
+        Mp3File mp3File = new Mp3File(file);
+        this.duration = mp3File.getLengthInMilliseconds();
+        this.frameCount = mp3File.getFrameCount();
+        this.player = new Player(new FileInputStream(file), audioDevice);
     }
 
     /**
@@ -39,7 +55,7 @@ public class PausablePlayer {
     public void play() {
         synchronized (playerLock) {
             switch (playerStatus) {
-                case NOTSTARTED:
+                case NOT_STARTED:
                     final Runnable r = this::playInternal;
                     final Thread t = new Thread(r);
                     t.setDaemon(true);
@@ -105,7 +121,7 @@ public class PausablePlayer {
             if (position - lastInvokedTimePosition >= 1000) {
 //                TODO: invoke label updater
                 lastInvokedTimePosition = position;
-                System.err.println(lastInvokedTimePosition);
+//                System.err.println(lastInvokedTimePosition);
             }
             // check if paused or terminated
             synchronized (playerLock) {
@@ -135,5 +151,15 @@ public class PausablePlayer {
         } catch (final Exception e) {
             // ignore, we are terminating anyway
         }
+    }
+
+    public void changePosition(int second) throws JavaLayerException {
+        this.pause();
+        try {
+            this.player = new Player(new FileInputStream(this.file));
+        } catch (JavaLayerException | FileNotFoundException ignored) {
+        }
+        player.skipMilliSeconds(second * 1000);
+        this.play();
     }
 }
