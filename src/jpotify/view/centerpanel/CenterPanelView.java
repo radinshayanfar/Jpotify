0042530a0dc0187e54b430807f446145aac100e5
Maryam Alikarami <9731045-9731097@ceit.local>
2019-06-24 01:31:49 +0430

    home added to the leftPanel

--- src/jpotify/view/centerpanel/CenterPanelView.java
+++ src/jpotify/view/centerpanel/CenterPanelView.java
@@ -15,9 +15,12 @@ public class CenterPanelView extends JPanel {
         setPreferredSize(new Dimension(WIDTH, MainView.HEIGHT));
         setLayout(new BorderLayout());
         this.setBackground(new Color(14,14, 14));
-        Border border1 = BorderFactory.createMatteBorder(10,10,10,10, new Color(14,14,14));
+
+        //nice border
+        Border border1 = BorderFactory.createMatteBorder(5,15,10,15, new Color(14,14,14));
         Border border2 = BorderFactory.createMatteBorder(1,0,0,0, Color.lightGray);
         this.setBorder(BorderFactory.createCompoundBorder(border1, border2));
+
         setVisible(true);
     }
 }
--- src/jpotify/view/leftpanel/LeftPanelView.java
+++ src/jpotify/view/leftpanel/LeftPanelView.java
@@ -1,9 +1,9 @@
 package jpotify.view.leftpanel;
 
 import jpotify.view.MainView;
-import jpotify.view.MyBorder;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class LeftPanelView extends JPanel {
@@ -15,36 +15,26 @@ public class LeftPanelView extends JPanel {
 //            , "SUMMER", "MOOD", "Ye chizi :|", "Ye chiz dige", "Zaheran khoobe!"));
 //    private ArtworkPanel artwork = new ArtworkPanel();
 
-    private MyBorder border1, border2, border3;
     private LibraryBar libraryBar;
     private PlaylistBar playlistBar;
     private ArtworkPanel artworkPanel;
-    private JPanel libraryBarAlignment;
-    private JPanel leftpanel = new JPanel();
 
     public LeftPanelView() {
 
         this.setLayout(new BorderLayout());
 
+        Border border1 = BorderFactory.createMatteBorder(2,15,4,15, Color.BLACK);
+        Border border2 = BorderFactory.createMatteBorder(0,0,1,0, Color.lightGray);
+
         //home
         JLabel home = new JLabel("Home");
         home.setFont(new Font("Arial", Font.PLAIN, 24));
         home.setForeground(Color.white);
+        home.setBorder(BorderFactory.createCompoundBorder(border1, border2));
         this.add(home, BorderLayout.NORTH);
-
-        libraryBarAlignment = new JPanel();
-        libraryBarAlignment.setPreferredSize(new Dimension(WIDTH, ELEMENTS_HEIGHT));
-        libraryBarAlignment.setBackground(Color.BLACK);
-        libraryBar = new LibraryBar();
-        libraryBarAlignment.add(libraryBar, BorderLayout.CENTER);
-
 
 
 //        this.add(new JScrollPane(libraryBar, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
-//        this.add(libraryBar);
-//        this.add(playlistBar);
-//        this.add(artwork);
-
         this.setPreferredSize(new Dimension(WIDTH, MainView.HEIGHT));
         this.setBackground(Color.black);
         this.setVisible(true);
