7d8343b70d21e77a487a77f3ab9c4018c309ccfe
Maryam Alikarami <9731045-9731097@ceit.local>
2019-06-26 16:03:21 +0430

    JplaylistSong created

--- src/jpotify/view/centerpanel/JPlaylistSong.java
+++ src/jpotify/view/centerpanel/JPlaylistSong.java
@@ -1,16 +1,16 @@
-//package jpotify.view.centerpanel;
-//
-//import jpotify.controller.MainController;
-//
-//public class JPlaylistSong {
-//    private MainController controller;
-//    private String album,artist, title;
-//
-//    public JPlaylistSong(MainController mainController, String title, String artist, String album) {
-//        this.controller = mainController;
-//        this.title = title;
-//        this.artist = artist;
-//        this./
-//    }
-//
-//}
+package jpotify.view.centerpanel;
+
+import jpotify.controller.MainController;
+
+public class JPlaylistSong {
+    private MainController controller;
+    private String album,artist, title;
+
+    public JPlaylistSong(MainController mainController, String title, String artist, String album) {
+        this.controller = mainController;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+    }
+
+}
